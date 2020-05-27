4f334f018c4d18e1d3e10ee26e1e1213f0225924
Przemysław Niedzielski <przemekniedzielski93@gmail.com>
2020-05-27 21:06:39 +0200

    add new tests

--- Framework/Pages/HomePage.cs
+++ Framework/Pages/HomePage.cs
@@ -1,5 +1,7 @@
 ﻿using Framework.Utils;
+using NUnit.Framework;
 using OpenQA.Selenium;
+using System;
 
 namespace Framework.Pages
 {
@@ -11,9 +13,44 @@ namespace Framework.Pages
         {
             driver = webDriver;
         }
+
+        //Elements
+        private readonly static string prevButton = "owl-prev";
+        private readonly static string nextButton = "owl-next";
+
+        //Methods
         public void GoToHomePage()
         {
             driver.Navigate().GoToUrl(Settings.url);
+        }
+
+        //Assertions
+        public void CheckIfPreviousButtonIsDisplayed()
+        {
+            IWebElement element = driver.FindElement(By.ClassName(prevButton));
+            try
+            {
+                Assert.IsTrue(element.Displayed);
+                Console.WriteLine($"{nameof(element)} button is displayed correctly.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"{nameof(element)} button is not displayed.");
+            }
+        }
+
+        public void CheckIfNextButtonIsDisplayed()
+        {
+            IWebElement element = driver.FindElement(By.ClassName(nextButton));
+            try
+            {
+                Assert.IsTrue(element.Displayed);
+                Console.WriteLine($"{nameof(element)} button is displayed correctly.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"{nameof(element)} button is not displayed.");
+            }
         }
     }
 }
